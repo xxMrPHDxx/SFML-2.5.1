2c731535750925c7a594434e27ab4ecf73c63a53
xxMrPHDxx <pian.minecraft1@gmail.com>
2020-10-16 17:11:35 +0000

    added update command

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
+sudo apt update
 sudo apt install libx11-dev libx11-dev libxrandr-dev libglu1-mesa-dev libgl-dev libudev-dev libfreetype-dev libopenal-dev libvorbis-dev libflac-dev
 cmake . && make
